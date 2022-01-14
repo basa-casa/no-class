e6c79bba58901d92d95b3cefa02dbe00744aa6e4
Kaushal Modi <kaushal.modi@gmail.com>
2022-01-14 17:17:45 -0500

    Default serve port to :2222

--- srv.sh
+++ srv.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+# Usage:
+#   ./srv.sh      # Serves on port 2222 by default
+#   ./srv.sh 1337 # Serves on port 1337
 
-hugo server --buildDrafts --navigateToChanged
+port="${1:-2222}" # default
+
+hugo server \
+     --buildDrafts \
+     --navigateToChanged \
+     --port "${port}"
