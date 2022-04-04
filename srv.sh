baa012544511222bb822d2a66294dc93e96610d9
Kaushal Modi <kaushal.modi@gmail.com>
2022-04-04 13:33:29 -0400

    Rewrite Hugo templates from the matarao repo with latest conventions
    Source of the Hugo template files:
    https://github.com/sirodoht/mataroa/tree/38e588ae3a11282e6c1fa9f911301ce5e41d5521/export_base_hugo
    
    Now only the style.css is directly mounted from the mataroa repo.

--- srv.sh
+++ srv.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 # Usage:
-#   ./srv.sh      # Serves on port 2222 by default
+#   ./srv.sh      # Serves on port 2213 by default
 #   ./srv.sh 1337 # Serves on port 1337
 
-port="${1:-2222}" # default
-
-# https://discourse.gohugo.io/t/replace-in-go-mod-works-but-hugo-module-replacements-env-var-doesnt-work/36746/10?u=kaushalmodi
-mod_base_dir=".."
-mods_to_be_replaced=("gitlab.com/kaushalmodi/hugo-mwe-theme")
-
-sed -i '/replace .* =>/d' go.mod
-for mod in "${mods_to_be_replaced[@]}"
-do
-    echo "replace ${mod} => ${mod_base_dir}/$(basename "${mod}")" >> go.mod
-done
+port="${1:-2213}" # default
 
 echo "Hugo Module Graph:"
 hugo mod graph
