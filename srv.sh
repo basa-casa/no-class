eade4961cbb5f93185fb2b36c4cff97735ae7d0d
Kaushal Modi <kaushal.modi@gmail.com>
2022-04-13 14:28:21 -0400

    chore: Change the default port number for hugo server

--- srv.sh
+++ srv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Usage:
-#   ./srv.sh      # Serves on port 2213 by default
+#   ./srv.sh      # Serves on port 2222 by default
 #   ./srv.sh 1337 # Serves on port 1337
 
-port="${1:-2213}" # default
+port="${1:-2222}" # default
 
 echo "Hugo Module Graph:"
 hugo mod graph
